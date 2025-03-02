a5cb6a2e922fac1b191cec0380961bedd0336254
Kiran-sai-hub <saikiran.vonteddu@gmail.com>
2025-03-02 16:19:22 +0530

    Fix: Serve frontend correctly

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'client.html'));
 });
 
 
